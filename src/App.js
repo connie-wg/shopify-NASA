f67aa4f597a890fbdb5e8a389289869aed39bf97
connor-li <77080536+connor-li@users.noreply.github.com>
2022-01-14 21:08:45 -0500

    firebase present but not functional

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
+
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+import { findByLabelText } from '@testing-library/react';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDsroF6o05l3jt89KWYzlEOv5fLsVcNjh0",
+  authDomain: "nasa-apod-web-app.firebaseapp.com",
+  projectId: "nasa-apod-web-app",
+  storageBucket: "nasa-apod-web-app.appspot.com",
+  messagingSenderId: "459467056132",
+  appId: "1:459467056132:web:6fbbb732e26ec49dd40151",
+  measurementId: "G-CHK38N1CW2"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles.background}>
+      
+        <div className='postContainer'>
+          <div className='imageContainer'>
+          <img className='imageInPost' src='http://clapway.com/wp-content/uploads/2016/03/8.-NASA.jpg'/>
+          </div>
+          
+          <div style={{display: 'flex', flex: 1}}>
+            <p>hi</p>
+          </div>
+        </div>
+   
     </div>
   );
 }
 
 export default App;
+
+const availHeight = window.innerHeight;
+const availWidth = window.innerWidth;
+
+const styles = {
+  background: {
+    backgroundColor: '#0F0E2C',
+    height: availHeight,
+    width: availWidth,
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    flex: 1,
+  }
+}
